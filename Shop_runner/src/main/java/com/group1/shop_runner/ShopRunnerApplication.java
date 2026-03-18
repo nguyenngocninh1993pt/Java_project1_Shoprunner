a5a986fad07a0b33ab6c70712c448d4def5dc2b1
Ninh-T2507E <ninhk47h3@gmail.com>
2026-03-18 11:25:16 +0700

    first commit

---
+++ Shop_runner/src/main/java/com/group1/shop_runner/ShopRunnerApplication.java
@@ -0,0 +1,13 @@
+package com.group1.shop_runner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopRunnerApplication.class, args);
+	}
+
+}
---
+++ Shop_runner/src/test/java/com/group1/shop_runner/ShopRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group1.shop_runner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopRunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
